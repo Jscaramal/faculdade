4a1c6c9134aa91f74be2ce378332f3c49c188a5d
Junior Scaramal <scaramaljunior@gmail.com>
2019-04-16 20:25:03 -0300

    FIRST COMMIT

---
+++ src/main/java/Aep/aepjava/App.java
@@ -0,0 +1,13 @@
+package Aep.aepjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
