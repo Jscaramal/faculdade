da75813320f30c229cd6faa727e5c66f31d57b01
Junior Scaramal <scaramaljunior@gmail.com>
2019-04-16 20:35:11 -0300

    Criando construtor da classe Aluno

--- src/main/java/Aep/aepjava/Aluno.java
+++ src/main/java/Aep/aepjava/Aluno.java
@@ -1,5 +1,13 @@
 package Aep.aepjava;
 
 public class Aluno {
-
+	private String nome; 
+	private int ra;
+	
+	public Aluno(int ra, String nome) {
+		this.ra = ra;
+		this.nome = nome;
+		
+	}
+	
 }
